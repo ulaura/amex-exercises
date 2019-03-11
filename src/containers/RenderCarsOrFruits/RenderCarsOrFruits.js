9d9c04b69247cf25477ce3d11e737f06f3763726
Laura Unaeze <l.unaeze5@gmail.com>
2019-03-10 17:38:34 -0700

    feat: Added basic dropdown for choosing cars or fruits.

--- src/containers/RenderCarsOrFruits/RenderCarsOrFruits.js
+++ src/containers/RenderCarsOrFruits/RenderCarsOrFruits.js
@@ -2,10 +2,38 @@ import React, { Component } from 'react';
 
 
 class RenderCarsOrFruits extends Component {
+  constructor(props) {
+    super(props); 
+
+    this.state = {
+      value: "Fruits"
+    }
+  }
+
+  handleOptionChange = e => {
+    this.setState({value: e.target.value});
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("You chose:", this.state.value);
+  }
+
   render() {
     return (
       <div>
         <h1>This is RenderCarsOrFruits Component</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Choose: Cars or Fruits?
+            <select value={this.state.value} onChange={this.handleOptionChange}>
+              <option value="Cars">Cars</option>
+              <option value="Fruits" defaultValue>Fruits</option>
+            </select>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
