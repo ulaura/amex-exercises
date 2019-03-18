26cf07b9f2c244ed60a41474efcbdb8ffacf1973
Laura Unaeze <l.unaeze5@gmail.com>
2019-03-18 16:19:42 -0700

    feat: Added css to the rest of home page.

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import './homepage.css'
 
 class HomePage extends Component {
   render() {
     return (
       <div>
         <h1 className="page-header">Amex React Challenge by Laura Unaeze</h1>
-        <p>Exercises:</p>
-        <a href="/rendermodalcounter">Render Modal Counter</a>
-        <br />
-        <a href="/rendertext">Render Text</a>
-        <br />
-        <a href="/rendercarsorfruits">Render Cars or Fruits</a>
+        <p id="home-message">Please use the buttons below or the navigation bar above to navigate to each exercise.</p>
+        <div id="home-buttons">
+          <a href="/rendermodalcounter" className="home-button">Render Modal Counter</a>
+          <a href="/rendertext" className="home-button">Render Text</a>
+          <a href="/rendercarsorfruits" className="home-button">Render Cars or Fruits</a>
+        </div>
       </div>
     )
   }
